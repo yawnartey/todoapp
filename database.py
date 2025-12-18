df1dcf148397c855ecb48b9e9f39aa22a40f2c43
Yaw Nartey <yaw.nartey@qubole.com>
2025-12-18 16:40:02 +0100

    update: moving to ec2 instance. using sqlite instead of postgres

--- database.py
+++ database.py
@@ -5,19 +5,19 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 #connect to sqlite
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 #connect to postgresql on local
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:enoch1234!@localhost/TodoApplicationDatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #connect to postgresql on local via docker
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    'DATABASE_URL', 
-    'postgresql://postgres:enoch1234!@host.docker.internal/TodoApplicationDatabase'
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = os.getenv(
+#     'DATABASE_URL', 
+#     'postgresql://postgres:enoch1234!@host.docker.internal/TodoApplicationDatabase'
+# )
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #this ensures local changes are not automatically commited to the databse
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
